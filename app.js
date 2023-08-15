235534fa36d8093bf520de9659d6928ca4a8406a
Jessica Almand <jlalmand27@gmail.com>
2023-08-15 16:31:15 -0400

    Changed ports from 3301 to 3001

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 3301;
+const PORT = process.env.PORT || 3001;
 // const server = http.createServer(process.env.PORT || 3000);
 const hbs = exphbs.create ({helpers});
 

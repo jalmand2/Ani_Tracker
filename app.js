890b9b27030daab5330d39a5514c085f0768301a
Jessica Almand <jlalmand27@gmail.com>
2023-08-15 01:39:28 -0400

    fixed connection session

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-store')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
 const app = express();
